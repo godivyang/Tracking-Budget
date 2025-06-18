fef41342ffc6ebd73c40e39ff141363f50827b3b
godivyang <godivyang@users.noreply.github.com>
2025-06-19 02:52:21 +0530

    bug fixes part 2

--- src/api/trackingBudget.js
+++ src/api/trackingBudget.js
@@ -12,7 +12,8 @@ const checkIfLogin = async () => {
         const response = await axiosInstance.get("/user/me");
         return response.data;
     } catch (e) {
-        window.location.href = process.env.REACT_APP_ULTIMATE_UTILITY_URL + "?redirect=TRACKING_BUDGET";
+        console.log(process.env.REACT_APP_ULTIMATE_UTILITY_URL);
+        // window.location.href = process.env.REACT_APP_ULTIMATE_UTILITY_URL + "?redirect=TRACKING_BUDGET";
     }
 };
 
